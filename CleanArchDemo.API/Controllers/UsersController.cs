9679f8a11469b4bd290b1d6abd923927d3ef5154
Shubham Kalegore <shubhamkalegore88@gmail.com>
2026-06-03 11:34:29 +0530

    changes related to cqrs and middler

--- CleanArchDemo.API/Controllers/UsersController.cs
+++ CleanArchDemo.API/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
-using CleanArchDemo.Application.Interfaces;
+using CleanArchDemo.Application.DTOs;
+using CleanArchDemo.Application.Features.Users.Commands;
+using CleanArchDemo.Application.Features.Users.Queries;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,17 +12,47 @@ namespace CleanArchDemo.API.Controllers;
 [Authorize(Roles = "Admin")]
 public class UsersController : ControllerBase
 {
-    private readonly IUserService _userService;
+    private readonly ISender _sender;
 
-    public UsersController(IUserService userService)
+    public UsersController(ISender sender)
     {
-        _userService = userService;
+        _sender = sender;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-        var users = await _userService.GetAllUsersAsync();
+        var users = await _sender.Send(new GetUsersQuery());
         return Ok(users);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<UserResponseDto>> GetById(Guid id)
+    {
+        var user = await _sender.Send(new GetUserByIdQuery(id));
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<UserResponseDto>> Create(UserDto request)
+    {
+        try
+        {
+            var user = await _sender.Send(new CreateUserCommand(
+                request.Email,
+                request.Password,
+                request.Role));
+
+            return CreatedAtAction(nameof(GetById), new { id = user.Id }, user);
+        }
+        catch (InvalidOperationException exception)
+        {
+            return BadRequest(exception.Message);
+        }
     }
 }
--- CleanArchDemo.Application/DependencyInjection.cs
+++ CleanArchDemo.Application/DependencyInjection.cs
@@ -10,6 +10,8 @@ public static class DependencyInjection
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(configuration =>
+            configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
         services.AddScoped<IProductService, ProductService>();
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<IUserService, UserService>();
